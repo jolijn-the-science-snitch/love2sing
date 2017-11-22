46b99fbdd223149526d7513828d43409b9c1ff03
Jolijn Quist <jolijnquist@live.nl>
2017-11-22 11:20:43 +0100

    Edited my pusername and password for the db

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -9,7 +9,7 @@ class DbHelper{
 
     //maakt de DB connectie aan
     public function __construct(){
-        $this -> connect = new PDO('mysql:host=localhost; dbname=love2sing;','root','');
+        $this -> connect = new PDO('mysql:host=localhost; dbname=love2sing;','wienerdoggo','123');
     } 
 
 ?>
