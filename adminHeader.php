85bbc2523ad0f7f5997f781aa087271e72760cd5
WimNijsink <whnijsink@gmail.com>
2017-11-30 15:50:48 +0100

    typfout gefixt

--- adminHeader.php
+++ adminHeader.php
@@ -78,7 +78,7 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-            <i class="fa fa-fw fa-sign-out"></i>Teurg naar website</a>
+            <i class="fa fa-fw fa-sign-out"></i>Terug naar website</a>
                 </li>
             </ul>
         </div>
